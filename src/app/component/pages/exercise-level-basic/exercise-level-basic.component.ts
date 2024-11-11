63e5d34c7230bfc6cbca85279a06157ed436aa58
vicpoo <157589559+vicpoo@users.noreply.github.com>
2024-11-11 13:36:07 -0600

    muscles

--- src/app/component/pages/exercise-level-basic/exercise-level-basic.component.ts
+++ src/app/component/pages/exercise-level-basic/exercise-level-basic.component.ts
@@ -4,12 +4,27 @@ import { SidebarComponent } from '../../sidebar/sidebar.component';
 import { HeaderComponent } from '../../header/header.component';
 import { BottomNavComponent } from '../../bottom-nav/bottom-nav.component';
 
-
 @Component({
-  selector: 'app-inicio',
+  selector: 'app-exercise-level-basic',
   standalone: true,
-  imports: [RouterModule, SidebarComponent,HeaderComponent, BottomNavComponent],  
+  imports: [RouterModule, SidebarComponent, HeaderComponent, BottomNavComponent],
   templateUrl: './exercise-level-basic.component.html',
   styleUrls: ['./exercise-level-basic.component.css']
 })
-export class ExerciseLevelBasicomponent {}
+export class ExerciseLevelBasicomponent {
+  exercises = [
+    { id: 1, title: 'BICEPS', image: 'assets/biceps.jpg' },
+    { id: 2, title: 'TRICEPS', image: 'assets/triceps.jpg' },
+    { id: 3, title: 'HOMBROS', image: 'assets/hombro.jpg' }
+  ];
+
+  currentExercise = 0;
+
+  nextExercise() {
+    this.currentExercise = (this.currentExercise + 1) % this.exercises.length;
+  }
+
+  prevExercise() {
+    this.currentExercise = (this.currentExercise - 1 + this.exercises.length) % this.exercises.length;
+  }
+}
